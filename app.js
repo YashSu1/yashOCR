6cb4f838d01380bb88a3973e392e052d122c991e
YashSu1 <yash@parallellearning.in>
2023-09-29 17:25:59 +0530

    No image croping

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const Tesseract = require('tesseract.js');
 const multer = require('multer');
-const Jimp = require('jimp'); // Import Jimp for image resizing
 const Webcam = require('node-webcam');
 const path = require('path');
 const fs = require('fs');
@@ -61,36 +60,26 @@ app.post('/save-photo', upload.single('photo'), (req, res) => {
 app.use(express.static(__dirname));
 
 // Handle POST request to /convert
-app.post('/convert', upload.single('image'), async (req, res) => {
+app.post('/convert', upload.single('image'), (req, res) => {
   const imageBuffer = req.file.buffer;
 
-  // Use Jimp to resize the image before OCR
-  try {
-    const image = await Jimp.read(imageBuffer);
-    const targetWidth = 800; // Adjust the width as needed
-    const resizedImageBuffer = await image.resize(targetWidth, Jimp.AUTO).getBufferAsync(Jimp.MIME_JPEG);
-
-    Tesseract.recognize(
-      resizedImageBuffer,
-      'eng',
-      {
-        logger: info => console.log(info),
-        tessedit_char_whitelist: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
-        psm: 7, // Try different page segmentation modes
-      }
-    ).then(({ data: { text } }) => {
-      // Perform post-processing to remove unwanted characters
-      const cleanedText = text.replace(/[`~!@#$%^&*()_=+\[\]{}\\|;:",<.>/?']/g, '');
-      console.log('Extracted Text:', cleanedText);
-      res.send(cleanedText);
-    }).catch(error => {
-      console.error('Error:', error);
-      res.status(500).send('Error occurred during OCR.');
-    });
-  } catch (error) {
-    console.error('Error resizing image:', error);
-    res.status(500).send('Error resizing image.');
-  }
+  Tesseract.recognize(
+    imageBuffer,
+    'eng',
+    {
+      logger: info => console.log(info),
+      tessedit_char_whitelist: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', // Restrict characters if needed
+      psm: 7, // Try different page segmentation modes
+    }
+  ).then(({ data: { text } }) => {
+    // Perform post-processing to remove unwanted characters
+    const cleanedText = text.replace(/[`~!@#$%^&*()_=+\[\]{}\\|;:",<.>/?']/g, '');
+    console.log('Extracted Text:', cleanedText);
+    res.send(cleanedText);
+  }).catch(error => {
+    console.error('Error:', error);
+    res.status(500).send('Error occurred during OCR.');
+  });
 
 });
 
@@ -99,5 +88,5 @@ app.listen(PORT, () => {
 });
 
 
-// Making changes manually_R1_R2_R3_R4_R5
+// Making changes manually_R1_R2_R3_R4
 
