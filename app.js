3434cf296af083c9d6c396c0f255f8e5b0d1d076
YashSu1 <yash@parallellearning.in>
2023-09-29 17:10:59 +0530

    Image crop to 800 px

--- app.js
+++ app.js
@@ -60,26 +60,36 @@ app.post('/save-photo', upload.single('photo'), (req, res) => {
 app.use(express.static(__dirname));
 
 // Handle POST request to /convert
-app.post('/convert', upload.single('image'), (req, res) => {
+app.post('/convert', upload.single('image'), async (req, res) => {
   const imageBuffer = req.file.buffer;
 
-  Tesseract.recognize(
-    imageBuffer,
-    'eng',
-    {
-      logger: info => console.log(info),
-      tessedit_char_whitelist: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', // Restrict characters if needed
-      psm: 7, // Try different page segmentation modes
-    }
-  ).then(({ data: { text } }) => {
-    // Perform post-processing to remove unwanted characters
-    const cleanedText = text.replace(/[`~!@#$%^&*()_=+\[\]{}\\|;:",<.>/?']/g, '');
-    console.log('Extracted Text:', cleanedText);
-    res.send(cleanedText);
-  }).catch(error => {
-    console.error('Error:', error);
-    res.status(500).send('Error occurred during OCR.');
-  });
+  // Use Jimp to resize the image before OCR
+  try {
+    const image = await Jimp.read(imageBuffer);
+    const targetWidth = 800; // Adjust the width as needed
+    const resizedImageBuffer = await image.resize(targetWidth, Jimp.AUTO).getBufferAsync(Jimp.MIME_JPEG);
+
+    Tesseract.recognize(
+      resizedImageBuffer,
+      'eng',
+      {
+        logger: info => console.log(info),
+        tessedit_char_whitelist: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
+        psm: 7, // Try different page segmentation modes
+      }
+    ).then(({ data: { text } }) => {
+      // Perform post-processing to remove unwanted characters
+      const cleanedText = text.replace(/[`~!@#$%^&*()_=+\[\]{}\\|;:",<.>/?']/g, '');
+      console.log('Extracted Text:', cleanedText);
+      res.send(cleanedText);
+    }).catch(error => {
+      console.error('Error:', error);
+      res.status(500).send('Error occurred during OCR.');
+    });
+  } catch (error) {
+    console.error('Error resizing image:', error);
+    res.status(500).send('Error resizing image.');
+  }
 
 });
 
@@ -88,5 +98,5 @@ app.listen(PORT, () => {
 });
 
 
-// Making changes manually_R1_R2_R3_R4
+// Making changes manually_R1_R2_R3_R4_R5
 
